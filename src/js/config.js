7e2b5e7a988a2591394b0ad873d8b6b557254543
gecemavisioa <gecemavisioa@hotmail.coma>
2021-05-07 22:55:49 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "d226876b-f1ad-43aa-9e76-e1f78a2944e2";
+export const MODAL_CLOSE_SEC = 2.5;
